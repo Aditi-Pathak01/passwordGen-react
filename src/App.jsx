5ac01a7d7777996145c5d209b7b44e8a9d2bcf66
Aditi-Pathak01 <pathakaditi322@gmail.com>
2024-06-20 17:23:11 +0530

    Hooks added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+/*
+(A)useCallbacks(()=>{},[]):- 
+(i)react hook that lets you cache a fx defination b/w re renders! 
+(ii)dependencies are written in form of array :- fOr optimization & caching .
+(B)useEffect(()=>{},[]):-
+(i) react hook which runs the fx automatically!
+(ii)dependencies are stored inside array and fx is run whenever the dependencies change!
+(C)useRef:-
+(i)lets us to reference a value!
+(ii)valriable has to be created to use useRef:-
+const someRef = useRef(null)
+ref={someRef} is passed as an attribute to the tag we want ref of,{for selection}!
+*/
 "useclient";
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState, useEffect,useRef } from "react";
 import "./App.css";
 
 function App() {
@@ -7,6 +20,7 @@ function App() {
   let [numAllowed, setNumAllowed] = useState(false);
   let [charAllowed, setCharAllowed] = useState(false);
   let [password, setPassword] = useState("");
+  const  passwordRef = useRef(null);
 
   const passwordGen = useCallback(() => {
     let pass = "";
@@ -18,11 +32,17 @@ function App() {
       pass += str.charAt(char);
     }
     setPassword(pass);
-  }, [length, numAllowed, charAllowed, setPassword]);
+  }, [length, numAllowed, charAllowed, setPassword]); //setPassword given here for method optimization.
+
+  const copyPasteToClipboard = () => {
+    passwordRef.current?.select();
+   // passwordRef.current?.setSelectionRange(0,6); it will select the range!
+    window.navigator.clipboard.writeText(password);
+  };
 
   useEffect(() => {
     passwordGen();
-  }, [length, numAllowed, charAllowed, passwordGen]);
+  }, [length, numAllowed, charAllowed, passwordGen]); //given here to run the fx again if dependency changes!
 
   return (
     <>
@@ -35,16 +55,22 @@ function App() {
             value={password}
             className="p-5 outline-none w-full rounded-2xl text-2xl text-gray-400"
             readOnly
+            ref={passwordRef}
           />
-          <button className="bg-gray-600 text-2xl rounded-2xl text-blue-900 font-bold">
+          <button
+            className="bg-gray-600 text-2xl rounded-2xl text-blue-900 font-bold"
+            onClick={() => {
+              copyPasteToClipboard();
+            }}
+          >
             Copy
           </button>
         </div>
-        <div className="bg-white flex gap-3 text-red-900">
+        <div className="flex gap-3 text-gray-400">
           <input
             type="range"
             min={8}
-            max={100}
+            max={12}
             value={length}
             onChange={(e) => {
               setLength(e.target.value);
